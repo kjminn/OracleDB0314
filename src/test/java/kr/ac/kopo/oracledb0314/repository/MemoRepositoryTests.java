f36ead1ca043ddbe0944fa192b34fb37fbbd1f3d
kjminn <kjminn2001@gmail.com>
2024-03-28 09:45:33 +0900

    페이징처리 2

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -95,6 +96,26 @@ public class MemoRepositoryTests {
         for (Memo memo : result.getContent()){
             System.out.println(memo);
         }
+
+        System.out.println("==================================================");
+
+        System.out.println("Total Pages: " + result.getTotalPages());
+        System.out.println("Total Count: " + result.getTotalElements());
+        System.out.println("Page Number: " + result.getNumber());
+        System.out.println("Page Size: " + result.getSize());
+        System.out.println("Has next page?: " + result.hasNext());
+        System.out.println("Is first page?: " + result.isFirst());
+
+    }
+
+    @Test
+    public void testSort(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println("number:"+memo.getMno() + ", content: " + memo.getMemoText());
+        });
     }
 
 }
